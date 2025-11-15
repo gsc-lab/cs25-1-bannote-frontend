56d127d07cc8cff2de719bd689008b6078bbe950
kyumin1227 <kyumin12271227@gmail.com>
2025-11-15 21:32:41 +0900

    feat: Register Page 추가
    - 기존의 Router 방식으로 import 할 경우 라우팅이 되지 않아 직접 App에 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, CustomRoutes, Resource } from "react-admin";
 import { AppLayout } from "./components/layout/AppLayout";
 import { dataProvider } from "./providers/dataProvider";
 import {
@@ -15,44 +15,44 @@ import { UserCreate, UserEdit, UserList } from "./pages/Users";
 import SchoolIcon from "@mui/icons-material/School";
 import ClassIcon from "@mui/icons-material/Class";
 import PersonIcon from "@mui/icons-material/Person";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import Router from "./Router";
+import { Route } from "react-router-dom";
 import { authProvider } from "./providers/authProvider";
 import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 export const App = () => (
-  <GoogleOAuthProvider clientId="616869349585-5kkpb0gvqlaq3kl4l67n6tq1nt6fee32.apps.googleusercontent.com">
-    <Admin
-      layout={AppLayout}
-      dataProvider={dataProvider}
-      authProvider={authProvider}
-      loginPage={LoginPage}
-    >
-      <Resource
-        name="departments"
-        list={DepartmentList}
-        create={DepartmentCreate}
-        edit={DepartmentEdit}
-        recordRepresentation="department_code"
-        icon={SchoolIcon}
-      />
-      <Resource
-        name="studentclasses"
-        list={StudentClassList}
-        create={StudentClassCreate}
-        edit={StudentClassEdit}
-        recordRepresentation="student_class_code"
-        icon={ClassIcon}
-      />
-      <Resource
-        name="users"
-        list={UserList}
-        create={UserCreate}
-        edit={UserEdit}
-        recordRepresentation="user_code"
-        icon={PersonIcon}
-      />
-      <Router />
-    </Admin>
-  </GoogleOAuthProvider>
+  <Admin
+    layout={AppLayout}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    loginPage={LoginPage}
+  >
+    <Resource
+      name="departments"
+      list={DepartmentList}
+      create={DepartmentCreate}
+      edit={DepartmentEdit}
+      recordRepresentation="department_code"
+      icon={SchoolIcon}
+    />
+    <Resource
+      name="studentclasses"
+      list={StudentClassList}
+      create={StudentClassCreate}
+      edit={StudentClassEdit}
+      recordRepresentation="student_class_code"
+      icon={ClassIcon}
+    />
+    <Resource
+      name="users"
+      list={UserList}
+      create={UserCreate}
+      edit={UserEdit}
+      recordRepresentation="user_code"
+      icon={PersonIcon}
+    />
+    <CustomRoutes noLayout>
+      <Route path="/register" element={<RegisterPage />} />
+    </CustomRoutes>
+  </Admin>
 );
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,3 @@
+export const RegisterPage = () => {
+  return <div>Registration page is under construction.</div>;
+};
