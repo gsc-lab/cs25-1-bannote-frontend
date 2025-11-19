1ef33c8bcedb56f0a34dc5c62de1fa6c128a2aa5
Saaatsuki <sasa46kaka@g.yju.ac.kr>
2025-11-19 17:34:29 +0900

    総合

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     host: true,
+        proxy: {
+      "/api": {
+        target: "http://210.101.236.160:8201",
+        changeOrigin: true,
+        secure: false,
+        cookieDomainRewrite: "",
+      },
+    },
   },
   build: {
     sourcemap: mode === "development",
